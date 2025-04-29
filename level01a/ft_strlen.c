7235224737816fc0a577feb9d8135df1738c1f04
g01100001 <gomandam@student.42madrid.com>
2025-04-29 13:39:13 +0200

    [add] review

---
+++ level01a/ft_strlen.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 12:54:16 by gomandam          #+#    #+#             */
+/*   Updated: 2025/04/29 13:05:25 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that returns the length of a string.
+
+Your function must be declared as follows:
+
+int     ft_strlen(char *str);
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+int	main(void)
+{
+	char *str;
+	
+	str = "abcde";
+	printf("%d", ft_strlen(str));
+}
+*/
