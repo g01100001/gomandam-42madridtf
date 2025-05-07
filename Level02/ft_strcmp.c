7c1eb84b4cb1a1aa0fbf18d12409f211dffac062
g01100001 <gomandam@student.42madrid.com>
2025-05-07 22:09:10 +0200

    [add] ft_strcmp.c

---
+++ Level02/ft_strcmp.c
@@ -0,0 +1,26 @@
+/*
+Assignment name  : ft_strcmp
+Expected files   : ft_strcmp.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strcmp (man strcmp).
+
+Your function must be declared as follows:
+
+int    ft_strcmp(char *s1, char *s2);
+*/
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (-1);
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
