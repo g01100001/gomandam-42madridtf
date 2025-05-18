312691a60701ccf89e192ebd3a25d8c38fcba5ee
g01100001 <gomandam@student.42madrid.com>
2025-05-19 00:05:05 +0200

    [add] 1_putnbr.c

---
+++ Level03/1_putnbr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:10:13 by gomandam          #+#    #+#             */
+/*   Updated: 2025/05/08 14:48:40 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putnbr(int nbr)
+{
+	char	c;
+
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	c = nbr % 10 + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int	n = 42000;
+	
+	ft_putnbr(n);
+}
